0f201318fb62abad957f54883d1b32e4438ceed4
gambrinus304 <devilhunter2009@mail.ru>
2018-03-20 02:43:55 +0300

    added first version of first task

---
+++ main.py
@@ -0,0 +1,9 @@
+friends = [
+    {'name': 'Nick', 'city': 'Milan', 'age': 23},
+    {'name': 'Kate', 'city': 'Paris', 'age': 19},
+    {'name': 'Sam', 'city': 'NewYork', 'age': 32}]
+
+
+def print_age(age):
+    print("Poehali!")
+    print(friends[1]['age'])
