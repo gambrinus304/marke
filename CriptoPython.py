68a8dc65588db5951c64faf00f555ee54a40f280
gambrinus304 <34950861+gambrinus304@users.noreply.github.com>
2018-03-25 23:21:19 +0300

    Stage 1 (#1)
    * killmeplz
    
    * added comments

---
+++ CriptoPython.py
@@ -0,0 +1,27 @@
+import requests
+
+url = "https://api.telegram.org/bot575720932: AAHoVwfr_CdPOG1919kI27t4KHMAtpXq_-g/"
+
+
+def get_bot_updates(limit, offset):
+    url = "https://api.telegram.org/bot575720932:AAHoVwfr_CdPOG1919kI27t4KHMAtpXq_-g/getUpdates"
+    params = {'limit': limit, 'offset': offset}
+    result = requests.get(url, params=params)
+    decoded = result.json()
+    return decoded['result']
+
+
+def send_message(chat_id, text):
+    url = "https://api.telegram.org/bot575720932:AAHoVwfr_CdPOG1919kI27t4KHMAtpXq_-g/sendMessage"
+    params = {'chat_id': chat_id, 'text': text}
+    response = requests.post(url, 'test', params=params)
+    return response
+
+
+result = get_bot_updates(5, 0)
+for update in result:
+    Update_message = update['message']
+    chat_id = update['message']['chat']['id']
+    last_message = update['update_id']
+    send_message(chat_id, 'test')
+# на данный момент это все, что получилось, к сожалению. Бот при запуске почему-то отсылает 5 сообщений - независимо от того, есть сообщения от пользователя или нет.
